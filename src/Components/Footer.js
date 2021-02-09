c15e62121219909e29d8b030982b6174aa15a901
R-Aniruddha <aniruddha04@gmail.com>
2021-02-09 15:14:37 +0100

    Updated Footer Animation

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Grid from '@material-ui/core/Grid'
 import { makeStyles } from '@material-ui/core/styles'
+import Tooltip from '@material-ui/core/Tooltip'
+import Fade from '@material-ui/core/Fade'
 import './Footer.css'
 
 const useStyles = makeStyles((theme) => ({
@@ -24,53 +26,98 @@ export default function Footer() {
         <Grid item xs={10} sm={6} lg={4} className='social-icon'>
           <Grid container>
             <Grid item xs={2} sm={1} lg={1} className='social-icon'>
-              <a
-                href='https://www.linkedin.com/in/aniruddha-ramakrishnan/'
-                rel='noopener noreferrer'
+              <Tooltip
+                TransitionComponent={Fade}
+                TransitionProps={{ timeout: 600 }}
+                interactive
+                arrow
+                placement='top'
+                title='LinkedIn'
               >
-                <img
-                  src='https://res.cloudinary.com/ani10/image/upload/v1612818121/Icons/linkedin-icon_wq6rol.svg'
-                  alt='LinkedIn icon'
-                />
-              </a>
+                <a
+                  href='https://www.linkedin.com/in/aniruddha-ramakrishnan/'
+                  rel='noopener noreferrer'
+                >
+                  <img
+                    src='https://res.cloudinary.com/ani10/image/upload/v1612818121/Icons/linkedin-icon_wq6rol.svg'
+                    alt='LinkedIn icon'
+                  />
+                </a>
+              </Tooltip>
             </Grid>
             <Grid item xs={2} sm={1} lg={1} className='social-icon'>
-              <a
-                href='https://github.com/R-Aniruddha'
-                rel='noopener noreferrer'
+              <Tooltip
+                TransitionComponent={Fade}
+                TransitionProps={{ timeout: 600 }}
+                interactive
+                arrow
+                placement='top'
+                title='GitHub'
               >
-                <img
-                  src='https://res.cloudinary.com/ani10/image/upload/v1612818307/Icons/github-icon-2_jfpuxc.svg'
-                  alt='GitHub icon'
-                />
-              </a>
+                <a
+                  href='https://github.com/R-Aniruddha'
+                  rel='noopener noreferrer'
+                >
+                  <img
+                    src='https://res.cloudinary.com/ani10/image/upload/v1612818307/Icons/github-icon-2_jfpuxc.svg'
+                    alt='GitHub icon'
+                  />
+                </a>
+              </Tooltip>
             </Grid>
             <Grid item xs={2} sm={1} lg={1} className='social-icon'>
-              <a
-                href='https://codepen.io/R-Aniruddha'
-                rel='noopener noreferrer'
+              <Tooltip
+                TransitionComponent={Fade}
+                TransitionProps={{ timeout: 600 }}
+                interactive
+                arrow
+                placement='top'
+                title='CodePen'
               >
-                <img
-                  src='https://res.cloudinary.com/ani10/image/upload/v1612863010/Icons/codepen-icon-5_muyaaw.svg'
-                  alt='Codepen icon'
-                />
-              </a>
+                <a
+                  href='https://codepen.io/R-Aniruddha'
+                  rel='noopener noreferrer'
+                >
+                  <img
+                    src='https://res.cloudinary.com/ani10/image/upload/v1612863010/Icons/codepen-icon-5_muyaaw.svg'
+                    alt='Codepen icon'
+                  />
+                </a>
+              </Tooltip>
             </Grid>
             <Grid item xs={2} sm={1} lg={1} className='social-icon'>
-              <a href='mailto:contact@r-aniruddha.com'>
-                <img
-                  src='https://res.cloudinary.com/ani10/image/upload/v1612817942/Icons/email-icon_ayoxnm.svg'
-                  alt='Email icon'
-                />
-              </a>
+              <Tooltip
+                TransitionComponent={Fade}
+                TransitionProps={{ timeout: 600 }}
+                interactive
+                arrow
+                placement='top'
+                title='Email'
+              >
+                <a href='mailto:contact@r-aniruddha.com'>
+                  <img
+                    src='https://res.cloudinary.com/ani10/image/upload/v1612817942/Icons/email-icon_ayoxnm.svg'
+                    alt='Email icon'
+                  />
+                </a>
+              </Tooltip>
             </Grid>
             <Grid item xs={2} sm={1} lg={1} className='social-icon'>
-              <a href='https://r-aniruddha.com' rel='noopener noreferrer'>
-                <img
-                  src='https://res.cloudinary.com/ani10/image/upload/v1612817667/Icons/webpage-icon_bflupl.svg'
-                  alt='Personal Website icon'
-                />
-              </a>
+              <Tooltip
+                TransitionComponent={Fade}
+                TransitionProps={{ timeout: 600 }}
+                interactive
+                arrow
+                placement='top'
+                title='Personal Website'
+              >
+                <a href='https://r-aniruddha.com' rel='noopener noreferrer'>
+                  <img
+                    src='https://res.cloudinary.com/ani10/image/upload/v1612817667/Icons/webpage-icon_bflupl.svg'
+                    alt='Personal Website icon'
+                  />
+                </a>
+              </Tooltip>
             </Grid>
           </Grid>
         </Grid>

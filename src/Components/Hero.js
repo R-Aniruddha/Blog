df513509b1a8f67050a09a75328d8f5761e48638
R-Aniruddha <aniruddha04@gmail.com>
2021-02-12 23:52:24 +0100

    Hero Image converted to webp format

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -5,7 +5,7 @@ import { makeStyles } from '@material-ui/core/styles'
 const useStyles = makeStyles((theme) => ({
   hero: {
     backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.7)), 
-    url('https://res.cloudinary.com/ani10/image/upload/v1612880966/Covers/code-background_xsxqul.jpg')`,
+    url('https://res.cloudinary.com/ani10/image/upload/v1613170178/Covers/code-background_me4bhf.webp')`,
     height: '45vh',
     backgroundPosition: 'center',
     backgroundRepeat: 'no-repeat',
